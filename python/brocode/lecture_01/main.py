f99a3b31ab28ab7bbcaa98bc788df6053757a5bc
Kaike Becher <becher.kaike@hotmail.com>
2026-05-07 10:15:02 -0300

    Reorganized files to document my learning progress

--- python/brocode/lecture_01/main.py
+++ python/brocode/lecture_01/main.py
@@ -1,4 +1,5 @@
 # This is NOT the first time I use a print...
 # This comment was added on Github for fetch test
+# It didn't worked as intended, used git pull
 
 print("Hi, I'm Kaike. I'll be a programmer soon!")
---
+++ python/brocode/readme.txt
@@ -0,0 +1,2 @@
+Learning my first programming language from this course, from BroCode Youtube's channel:
+https://www.youtube.com/watch?v=ix9cRaBkVe0
