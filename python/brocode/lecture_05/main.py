912cace39c5fbb71f6ee789c97c9fe771bd9f741
Kaike Becher <becher.kaike@hotmail.com>
2026-05-10 02:25:13 -0300

    Lesson 5 - arithmetics and math

---
+++ python/brocode/lecture_05/main.py
@@ -0,0 +1,46 @@
+import math # imports the math module and its functions
+
+# arithmetic and math
+
+# augmented operators
+
+friends = 2
+
+# friends = friends + 1
+# friends += 1
+
+# friends = friends - 1
+# friends -= 1
+
+# friends = friends * 2
+# friends *= 2
+
+# friends = friends / 2 (returns a float)
+# friends /= 2
+
+# friends = friends ** 2
+# friends **= 2
+
+# friends = friends % 2
+# friends %= 2
+
+# print(friends)
+
+x = 3.14
+y = -4
+z = 5
+
+# result = round(x)
+# result = abs(y) absolute function
+# result = pow(4, 3) power function
+# result = max (x, y, z) returns the max value between various values
+# result = min(x, y, z) returns the max value between various values
+# print(result)
+
+
+# print(math.pi)
+# print(math.e)
+# print(math.sqrt(9)) # square root
+# print(math.ceil(3.14)) # always rounds a floating number up
+# print(math.floor(3.9)) # always rounds a floating number down
+
